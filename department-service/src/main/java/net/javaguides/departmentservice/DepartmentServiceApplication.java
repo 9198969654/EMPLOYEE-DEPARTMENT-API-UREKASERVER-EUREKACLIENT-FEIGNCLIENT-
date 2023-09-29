8ad8f778c4aea86766cf38b6c4071a350159de99
User <narendrakumarnishad56@gmail.com>
2023-09-29 23:00:12 +0530

    Merge remote-tracking branch 'origin/master'

--- department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
+	//hi
 }
